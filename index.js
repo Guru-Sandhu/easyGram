8126bf3c16fcee5123103025601dfbb65a9bea65
Guru-Sandhu <arsh3874@gmail.com>
2020-03-10 13:27:40 -0700

    made EasyGram

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
 const express = require('express')
 const logger = require('morgan')
 
-const app = express();
+const app = express()
 
 app.use(logger('dev'))
 
-app.get('/', (req,res) => {
-  res.send('Hello World')
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  res.render('welcome')
 })
 
-const PORT = 4000;
-const DOMAIN = 'localhost';
-app.listen(PORT, DOMAIN , () => {
-  console.log(`Server Listening on ${DOMAIN}:${PORT}`)
+app.get('/users/new', (req, res) => {
+  res.render('users/new.ejs')
+})
+
+app.post('/users', (req, res) => {
+  const {
+    firstname,
+    lastname,
+    email,
+    password,
+    passwordConfirmation
+  } = req.body
+  if (password === passwordConfirmation) {
+    res.send(`
+    Thanks for signing up ${firstname} ${lastname}! We'll make sure we keep your password: ${password} and email: ${email} safe!
+    `)
+  } else {
+    res.render('users/new.ejs')
+  }
+})
+
+const PORT = 4000
+const DOMAIN = 'localhost'
+
+app.listen(PORT, DOMAIN, () => {
+  console.log(`Listening on ${DOMAIN}:${PORT}`)
 })
