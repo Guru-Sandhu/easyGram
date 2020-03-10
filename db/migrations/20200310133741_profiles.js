51ce40e94d19d6e96e63d059ce414868ec39ddd6
Guru-Sandhu <arsh3874@gmail.com>
2020-03-10 14:08:49 -0700

    setup knex file and created table using it

---
+++ db/migrations/20200310133741_profiles.js
@@ -0,0 +1,14 @@
+// const knex = require('knex')
+
+exports.up = function (knex) {
+  return knex.schema.createTable('grams', t => {
+    t.increments('id')
+    t.string('srcUrl')
+    t.string('description')
+    t.timestamps(true, true)
+  })
+}
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('grams')
+}
---
+++ knexfile.js
@@ -0,0 +1,17 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: 'easygram',
+      username: 'guru',
+      password: 'Sandhu@123'
+    },
+    migrations: {
+      directory: './db/migrations',
+      tableName: 'migration'
+    }
+  }
+}
